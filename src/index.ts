083665d59d221e2854c865b2a2b8d5ef51343797
jclewis33 <109756608+jclewis33@users.noreply.github.com>
2023-03-17 18:01:43 -0500

    Updated code to pull from API

--- src/index.ts
+++ src/index.ts
@@ -1,66 +1,149 @@
 window.Webflow = window.Webflow || [];
 window.Webflow.push(() => {
   const api_url =
-    'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=USD&include_market_cap=true&include_24hr_vol=true&include_24hr_change=true&include_last_updated_at=true';
+    'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&ids=ref-finance&order=market_cap_desc&per_page=100&page=1&sparkline=false';
 
   async function getData() {
     try {
       const response = await fetch(api_url);
       const data = await response.json();
-      // access the ethereum object from api
-      const ethereum = data.ethereum;
 
-      // access the value of usd from api
-      const usd = ethereum.usd;
+      // access the value of price from api
+      const price = data[0].current_price;
 
-      // check the value of usd and format it accordingly
-      let formattedUsd = '';
-      if (usd >= 1000000000) {
-        formattedUsd = (usd / 1000000000).toFixed(1) + ' B';
-      } else if (usd >= 1000000) {
-        formattedUsd = (usd / 1000000).toFixed(1) + ' M';
-      } else if (usd >= 1000) {
-        formattedUsd = (usd / 1000).toFixed(1) + ' K';
+      // check the value of price and format it accordingly
+      let formattedPrice = '';
+      if (price === null) {
+        formattedPrice = 'N/A';
+      } else if (price >= 1000000000) {
+        formattedPrice = '$ ' + (price / 1000000000).toFixed(1) + ' B';
+      } else if (price >= 1000000) {
+        formattedPrice = '$ ' + (price / 1000000).toFixed(1) + ' M';
+      } else if (price >= 1000) {
+        formattedPrice = '$ ' + (price / 1000).toFixed(1) + ' K';
       } else {
-        formattedUsd = usd;
+        formattedPrice = `$ ${price} USD`;
       }
 
-      // access the value of usd_market_cap from api
-      const usdMarketCap = ethereum.usd_market_cap;
+      // access the value of market_cap from api
+      const marketCap = data[0].market_cap;
 
-      // check the value of usdMarketCap and format it accordingly
-      let formattedUsdMarketCap = '';
-      if (usdMarketCap >= 1000000000) {
-        formattedUsdMarketCap = (usdMarketCap / 1000000000).toFixed(1) + ' B';
-      } else if (usdMarketCap >= 1000000) {
-        formattedUsdMarketCap = (usdMarketCap / 1000000).toFixed(1) + ' M';
-      } else if (usdMarketCap >= 1000) {
-        formattedUsdMarketCap = (usdMarketCap / 1000).toFixed(1) + ' K';
+      // check the value of marketCap and format it accordingly
+      let formattedMarketCap = '';
+      if (marketCap === null) {
+        formattedMarketCap = 'N/A';
+      } else if (marketCap >= 1000000000) {
+        formattedMarketCap = (marketCap / 1000000000).toFixed(1) + ' B';
+      } else if (marketCap >= 1000000) {
+        formattedMarketCap = (marketCap / 1000000).toFixed(1) + ' M';
+      } else if (marketCap >= 1000) {
+        formattedMarketCap = (marketCap / 1000).toFixed(1) + ' K';
       } else {
-        formattedUsdMarketCap = usdMarketCap;
+        formattedMarketCap = marketCap;
       }
 
-      // access the value of usd_24h_vol from api
-      const usdTradingVol = ethereum.usd_24h_vol; //usdTradingVol is the 24hour trading volume
+      // access the value of high_24h from api
+      const high24h = data[0].high_24h; //high_24h trading volume in 24hour period
+      // access the value of high_24h from api
+      const low24h = data[0].low_24h; //low_24h trading volume in 24hour period
+      // average high_24h and low_24h to get average tradingVol
+      const tradingVol = (high24h + low24h) / 2; //average trading vol in 24hour period
 
-      // check the value of usdTradingVol and format it accordingly
-      let formattedUsdTradingVol = '';
-      if (usdTradingVol >= 1000000000) {
-        formattedUsdTradingVol = (usdTradingVol / 1000000000).toFixed(1) + ' B';
-      } else if (usdTradingVol >= 1000000) {
-        formattedUsdTradingVol = (usdTradingVol / 1000000).toFixed(1) + ' M';
-      } else if (usdTradingVol >= 1000) {
-        formattedUsdTradingVol = (usdTradingVol / 1000).toFixed(1) + ' K';
+      // check the value of tradingVol and format it accordingly
+      let formattedTradingVol = '';
+      if (tradingVol === null) {
+        formattedTradingVol = 'N/A';
+      } else if (tradingVol >= 1000000000) {
+        formattedTradingVol = (tradingVol / 1000000000).toFixed(1) + ' B';
+      } else if (tradingVol >= 1000000) {
+        formattedTradingVol = (tradingVol / 1000000).toFixed(1) + ' M';
+      } else if (tradingVol >= 1000) {
+        formattedTradingVol = (tradingVol / 1000).toFixed(1) + ' K';
       } else {
-        formattedUsdTradingVol = usdTradingVol;
+        formattedTradingVol = tradingVol;
       }
 
-      // update the usd element in Webflow with the formatted USD value
-      document.getElementById('usd').textContent = formattedUsd;
-      // update the market-cap element in Webflow with the formattedUsdMarketCap value
-      document.getElementById('market-cap').textContent = formattedUsdMarketCap;
-      // update the trading-vol element in Webflow with the formattedUsdTradingVol value
-      document.getElementById('trading-vol').textContent = formattedUsdTradingVol;
+      // access the value of fully_diluted_valuation from api
+      const dilutedValuation = data[0].fully_diluted_valuation;
+
+      // check the value of dilutedValuation and format it accordingly
+      let formattedDilutedValuation = '';
+      if (dilutedValuation === null) {
+        formattedDilutedValuation = 'N/A';
+      } else if (dilutedValuation >= 1000000000) {
+        formattedDilutedValuation = (dilutedValuation / 1000000000).toFixed(1) + ' B';
+      } else if (dilutedValuation >= 1000000) {
+        formattedDilutedValuation = (dilutedValuation / 1000000).toFixed(1) + ' M';
+      } else if (dilutedValuation >= 1000) {
+        formattedDilutedValuation = (dilutedValuation / 1000).toFixed(1) + ' K';
+      } else {
+        formattedDilutedValuation = dilutedValuation;
+      }
+
+      // access the value of circulating_supply from api
+      const circulatingSupply = data[0].circulating_supply;
+
+      // check the value of circulatingSupply and format it accordingly
+      let formattedCirculatingSupply = '';
+      if (circulatingSupply === null) {
+        formattedCirculatingSupply = 'N/A';
+      } else if (circulatingSupply >= 1000000000) {
+        formattedCirculatingSupply = (circulatingSupply / 1000000000).toFixed(1) + ' B';
+      } else if (circulatingSupply >= 1000000) {
+        formattedCirculatingSupply = (circulatingSupply / 1000000).toFixed(1) + ' M';
+      } else if (circulatingSupply >= 1000) {
+        formattedCirculatingSupply = (circulatingSupply / 1000).toFixed(1) + ' K';
+      } else {
+        formattedCirculatingSupply = circulatingSupply;
+      }
+
+      // access the value of total_supply from api
+      const totalSupply = data[0].total_supply;
+
+      // check the value of totalSupply and format it accordingly
+      let formattedTotalSupply = '';
+      if (totalSupply === null) {
+        formattedTotalSupply = 'N/A';
+      } else if (totalSupply >= 1000000000) {
+        formattedTotalSupply = (totalSupply / 1000000000).toFixed(1) + ' B';
+      } else if (totalSupply >= 1000000) {
+        formattedTotalSupply = (totalSupply / 1000000).toFixed(1) + ' M';
+      } else if (totalSupply >= 1000) {
+        formattedTotalSupply = (totalSupply / 1000).toFixed(1) + ' K';
+      } else {
+        formattedTotalSupply = totalSupply;
+      }
+
+      // access the value of max_supply from api
+      const maxSupply = data[0].max_supply;
+
+      // check the value of maxSupply and format it accordingly
+      let formattedMaxSupply = '';
+      if (maxSupply === null) {
+        formattedMaxSupply = 'N/A';
+      } else if (maxSupply >= 1000000000) {
+        formattedMaxSupply = (maxSupply / 1000000000).toFixed(1) + ' B';
+      } else if (maxSupply >= 1000000) {
+        formattedMaxSupply = (maxSupply / 1000000).toFixed(1) + ' M';
+      } else if (maxSupply >= 1000) {
+        formattedMaxSupply = (maxSupply / 1000).toFixed(1) + ' K';
+      } else {
+        formattedMaxSupply = maxSupply;
+      }
+      // update the price element in Webflow with the formattedPrice value
+      document.getElementById('price').textContent = formattedPrice;
+      // update the market-cap element in Webflow with the formattedMarketCap value
+      document.getElementById('market-cap').textContent = formattedMarketCap;
+      // update the trading-vol element in Webflow with the formattedTradingVol value
+      document.getElementById('trading-vol').textContent = formattedTradingVol;
+      // update the diluted-valuation element in Webflow with the formattedDilutedValuation value
+      document.getElementById('diluted-valuation').textContent = formattedDilutedValuation;
+      // update the circulating-supply element in Webflow with the formattedCiculatingSupply value
+      document.getElementById('circulating-supply').textContent = formattedCirculatingSupply;
+      // update the total-supply element in Webflow with the formattedTotalSupply value
+      document.getElementById('total-supply').textContent = formattedTotalSupply;
+      // update the max-supply element in Webflow with the formattedMaxSupply value
+      document.getElementById('max-supply').textContent = formattedMaxSupply;
     } catch (error) {
       console.log(error);
     }
@@ -71,15 +154,3 @@ window.Webflow.push(() => {
     // calling getData() at regular intervals to update the market cap periodically
     setInterval(getData, 60000); // 60000 ms = 1 minute
 });
-
-/*if (usdMarketCap >= 1000000000) {
-  return (usdMarketCap / 1000000000).toFixed(1) + ' B';
-}
-if (usdMarketCap >= 1000000) {
-  return (usdMarketCap / 1000000).toFixed(1) + ' M';
-}
-if (usdMarketCap >= 1000) {
-  return (usdMarketCap / 1000).toFixed(1) + ' K';
-}
-return usdMarketCap;
-*/
